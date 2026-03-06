0ba12ca4b967d7b161f1c602c7facf173be2a8f5
艾莉丝努力练剑 <18106882575@163.com>
2026-03-07 00:39:21 +0800

    QWidget_8

--- code/QWidget_8/widget.cpp
+++ code/QWidget_8/widget.cpp
@@ -1,5 +1,6 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QDebug>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
@@ -13,3 +14,29 @@ Widget::~Widget()
     delete ui;
 }
 
+void Widget::on_pushButton_add_clicked()
+{
+    // 在这里完成不透明度的修改
+    float opacity = this->windowOpacity();
+    if(opacity >= 1.0)
+    {
+        return;
+    }
+    qDebug() << opacity;
+    opacity += 0.1; // 数值变大意味着不透明度增加了
+    // 把修改之后的值重新设置进去
+    this->setWindowOpacity(opacity);
+}
+
+void Widget::on_pushButton_sub_clicked()
+{
+    // 先获取到当前的opacity的值
+    float opacity = this->windowOpacity();
+    if(opacity <= 0.0)
+    {
+        return;
+    }
+    qDebug() << opacity;
+    opacity -= 0.1;
+    this->setWindowOpacity(opacity);
+}

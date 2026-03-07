4e481947d166f168ebf237618fe24088bb7c9796
艾莉丝努力练剑 <18106882575@163.com>
2026-03-08 00:02:36 +0800

    QWidget_8

--- code/QWidget_8/widget.cpp
+++ code/QWidget_8/widget.cpp
@@ -18,10 +18,10 @@ void Widget::on_pushButton_add_clicked()
 {
     // 在这里完成不透明度的修改
     float opacity = this->windowOpacity();
-    if(opacity >= 1.0)
-    {
-        return;
-    }
+//    if(opacity >= 1.0)
+//    {
+//        return;
+//    }
     qDebug() << opacity;
     opacity += 0.1; // 数值变大意味着不透明度增加了
     // 把修改之后的值重新设置进去
@@ -32,10 +32,10 @@ void Widget::on_pushButton_sub_clicked()
 {
     // 先获取到当前的opacity的值
     float opacity = this->windowOpacity();
-    if(opacity <= 0.0)
-    {
-        return;
-    }
+//    if(opacity <= 0.0)
+//    {
+//        return;
+//    }
     qDebug() << opacity;
     opacity -= 0.1;
     this->setWindowOpacity(opacity);
